045b9b860ef74562f6319af9a5e40d6c574e2f84
crispymccoy145 <samspam19941@gmail.com>
2021-05-07 12:48:47 +0100

    added css

--- js/app.js
+++ js/app.js
@@ -9,8 +9,30 @@ const addTaskToList = function () {
     event.preventDefault();
     const taskTitle = event.target.title.value;
     const taskDescription = event.target.description.value;
-    const taskDate = event.target.date.value;
-    console.log(taskTitle);
-    console.log(taskDescription);
-    console.log(taskDate.split('-').reverse().join('-'));
+    const taskDate = event.target.date.value.split('-').reverse().join('/');
+
+    const list = document.querySelector('ul');
+    const form = document.querySelector('form')
+
+    const newDivElement = document.createElement('div')
+
+    const newListElementTitle = document.createElement('li');
+    const newListElementDescription = document.createElement('li');
+    const newListElementDate = document.createElement('li');
+
+    newListElementTitle.textContent = `${taskTitle}`;
+    newListElementDescription.textContent = `${taskDescription}`;
+    newListElementDate.textContent = `${taskDate}`;
+
+    newDivElement.className = 'tasks'
+
+    newDivElement.appendChild(newListElementTitle);
+    newDivElement.appendChild(newListElementDescription);
+    newDivElement.appendChild(newListElementDate);   
+    list.appendChild(newDivElement);
+
+    // form.reset();
+    
+    
+    
 };
