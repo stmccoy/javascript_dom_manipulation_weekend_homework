a45d69745e7fad1df571497c9cc76ad54048ea2f
crispymccoy145 <samspam19941@gmail.com>
2021-05-07 11:43:23 +0100

    date reversed, all inputs displaying to console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('hello  world')
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#form-content');
+
+    form.addEventListener('submit', addTaskToList);
+});
+
+
+const addTaskToList = function () {
+    event.preventDefault();
+    const taskTitle = event.target.title.value;
+    const taskDescription = event.target.description.value;
+    const taskDate = event.target.date.value;
+    console.log(taskTitle);
+    console.log(taskDescription);
+    console.log(taskDate.split('-').reverse().join('-'));
+};
